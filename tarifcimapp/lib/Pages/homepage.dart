fd0e9da968507621d38c349330f34b84236540f6
ummuhnoksuz <ummuhanoksuz1@gmail.com>
2020-10-14 02:21:32 +0300

    first commit-homepage

---
+++ tarifcimapp/lib/Pages/homepage.dart
@@ -0,0 +1,143 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:tarifcimapp/widgets/homepage_card_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+        Container(
+          child: Card(
+            color: Colors.white,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Image.asset("assets/images/anasayfa/avocado.png"),
+                  flex: 1,
+                ),
+                Expanded(
+                  child: Text(
+                    "Tarifçim",
+                    style: TextStyle(fontSize: 40, fontFamily: "baslik"),
+                  ),
+                  flex: 3,
+                ),
+                Expanded(
+                  child: Icon(Icons.share),
+                ),
+                Expanded(
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ),
+                    onPressed: () {},
+                    // onPressed: showDialog(
+                    //     context: context,
+                    //     builder: (_) => new AlertDialog(
+                    //           title: new Text("Material Dialog"),
+                    //           content: new Text("Hey! I'm Coflutter!"),
+                    //           actions: <Widget>[
+                    //             FlatButton(
+                    //               child: Text('Close me!'),
+                    //               onPressed: () {
+                    //                 Navigator.of(context).pop();
+                    //               },
+                    //             )
+                    //           ],
+                    //         )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        new Container(
+          child: new GridView.count(
+            crossAxisCount: 3,
+            controller: new ScrollController(keepScrollOffset: false),
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            children: [
+              HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/sandwich.png"),
+                cardText: "Atıştırmalıklar",
+              ),
+             HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/cola.png"),
+                cardText: "İçecekler",
+              ),
+              HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/pie.png"),
+                cardText: "Pastalar",
+              ),
+             HomePageCardWidget(
+                cardImage:
+                    Image.asset("assets/images/anasayfa/gingerbreadman.png"),
+                cardText: "Kurabiyeler",
+              ),
+             HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/avocado.png"),
+                cardText: "Diyet Yemekleri",
+              ),
+              HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/cutlery.png"),
+                cardText: "Ev Yemekleri",
+              ),
+            HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/broccoli.png"),
+                cardText: "Salatalar",
+              ),
+               HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/bread.png"),
+                cardText: "Hamur işi",
+              ),
+                HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/fish.png"),
+                cardText: "Balık",
+              ),
+                HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/icecream.png"),
+                cardText: "Dondurmalar",
+              ),
+             HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/noodles.png"),
+                cardText: "Makarnalar",
+              ),
+               HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/ketchup.png"),
+                cardText: "Soslar",
+              ),
+               HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/jam.png"),
+                cardText: "Reçeller",
+              ),
+               HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/coffee.png"),
+                cardText: "Kahveler",
+              ),
+               HomePageCardWidget(
+                cardImage: Image.asset("assets/images/anasayfa/burger.png"),
+                cardText: "Fastfood",
+              ),
+            ],
+          ),
+        ),
+      ]),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.red[300],
+        items: <Widget>[
+          Icon(Icons.lightbulb, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.add, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
+      ),
+    );
+  }
+}
---
+++ tarifcimapp/lib/widgets/homepage_card_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomePageCardWidget extends StatelessWidget {
+  final String cardText;
+  final Widget cardImage;
+
+  const HomePageCardWidget({
+    Key key,
+    this.cardText,
+    this.cardImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+   
+        shape: BeveledRectangleBorder(side: BorderSide(color: Colors.red)),
+        color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            Expanded(
+              child: CircleAvatar(
+                radius: 80,
+                child: cardImage,
+              ),
+              flex: 5,
+            ),
+            Expanded(
+              child: Text(
+                cardText,
+                style: TextStyle(fontFamily: "metin", fontSize: 17),
+              ),
+              flex: 2,
+            )
+          ],
+        ),
+      ),
+      decoration: new BoxDecoration(
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            blurRadius: 20.0, // soften the shadow
+            spreadRadius: 0.0, //extend the shadow
+            offset: Offset(
+              5.0, // Move to right 10  horizontally
+              5.0, // Move to bottom 10 Vert
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
